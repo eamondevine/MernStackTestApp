e03ac8ae92844a726cf32cd7a785eda4ad1839ba
eamondevine <eamondevine@gmail.com>
2025-02-10 15:13:24 +0800

    DON'T RUN NPM INSTALL IN THE MAIN FOLDER!! ok?

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      //makes the fetch from the backend api (see server file with .use method)
+      const response = await fetch("http://localhost:4000/api/blogs");
+      const json = response.json();
+
+      if (response.ok) {
+        //this is an array of workouts gotten from the blog controller file in the back
+        setBlogs(json);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
   return (
     <div className="home">
-      <h2>home</h2>
+      <div className="blogs"></div>
     </div>
   );
 };
