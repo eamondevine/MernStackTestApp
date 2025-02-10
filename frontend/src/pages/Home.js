7c597520ab050da719af2074b41c99cd91ff2130
eamondevine <eamondevine@gmail.com>
2025-02-10 15:42:44 +0800

    I'm at the 9 min mark on vid 9. Couldn't run npm run dev on the backend to get to mongo, just added ip address, but maybe it takes a while?

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       //makes the fetch from the backend api (see server file with .use method)
-      const response = await fetch("http://localhost:4000/api/blogs");
+      //originally doing a cross origin, ran into error, proxy def added to json file
+      const response = await fetch("/api/blogs");
       const json = response.json();
 
       if (response.ok) {
@@ -19,7 +20,9 @@ const Home = () => {
 
   return (
     <div className="home">
-      <div className="blogs"></div>
+      <div className="blogs">
+        {blogs && blogs.map((blog) => <p key={blog._id}>{blog.title}</p>)}
+      </div>
     </div>
   );
 };
