df61d2f397efbb92ab9763f5131c798154027276
eamondevine <eamondevine@gmail.com>
2025-03-12 11:21:39 +0800

    Finished video 4 fixed bug (request and response were mixed up) and I have validator working to make sure email is a proper email address and password is strong. Don't know how to make options for password yet.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,14 @@
 const User = require("../models/userModel");
 
 // login user
-const loginUser = async (res, req) => {
+const loginUser = async (req, res) => {
   res.json({ mssg: "login user" });
 };
 
 // signup user
 // if confused about the props here, go watch 11 min mark of
 // net ninja mern auth vid 3
-const signupUser = async (res, req) => {
+const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,17 @@ const userSchema = new Schema({
 //static signup method
 // can't use arrows with this.
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
