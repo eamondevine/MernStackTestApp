0e06fb24cefbdae53c3846ca01385bb95fac8aae
eamondevine <eamondevine@gmail.com>
2025-03-16 19:27:13 +0800

    Finished video 7 of mern auth net ninja Did the login and signup hashing and logic, also installed jsonwebtokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// calling it _id because that's what mongodb is calling it
+const createToken = (_id) => {
+  //sign method used with id: "string"
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
@@ -12,7 +28,9 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -42,4 +42,23 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+  // password is the plain text, and user.password is the user object
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
