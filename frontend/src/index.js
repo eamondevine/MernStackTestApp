f074753cf0fd68a4bcdbbaebaf6cc47424b811e8
eamondevine <eamondevine@gmail.com>
2025-03-17 09:03:38 +0800

    Finished video 8 net ninja mern Auth Completed the create and use context for global auth context throughout the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BlogsContextProvider } from "./context/BlogContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BlogsContextProvider>
-      <App />
-    </BlogsContextProvider>
+    <AuthContextProvider>
+      <BlogsContextProvider>
+        <App />
+      </BlogsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
