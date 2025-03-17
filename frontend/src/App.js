4da0b65e1aafd8a19d2b3441e364ada52d1d619f
eamondevine <eamondevine@gmail.com>
2025-03-17 10:33:57 +0800

    Finished video 9 on net ninja mern auth Created the login and signup pages on the frontend. Did a little CSS styling as well: spacing and max-width.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Browser, Routes, Route } from "react-router-dom";
 // pages and components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Browser>
