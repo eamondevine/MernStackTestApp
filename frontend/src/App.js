ccaeb0f936d0bd27903e9dc0a1ec38c4efeea4ec
eamondevine <eamondevine@gmail.com>
2025-03-23 20:11:55 +0800

    Finished vid 16 mern net ninja auth Used Navigator tag from react router dom in the app.js file to redirect users with a ternary operator so that they can't see certain pages whilst logged in or out.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,36 @@
-import { BrowserRouter as Browser, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Browser,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 // pages and components
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
+import { use } from "react";
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <Browser>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </Browser>
